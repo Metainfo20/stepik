1724e4c09837de36d8c01f32d8f3c29c5fc8f9d4
metainfo20 <metainfo20@gmail.com>
2021-09-16 23:11:55 +0300

    add link variable

--- test_items.py
+++ test_items.py
@@ -1,8 +1,10 @@
 import time
 
+link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+
 
 def test_button_add_to_basket_is_exist(browser):
-    browser.get("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/")
+    browser.get(link)
     time.sleep(30)
     assert browser.find_element_by_class_name(
         "btn-add-to-basket"
