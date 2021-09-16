24d921d545bd1270a5b48b5550fd93a6482b90f6
metainfo20 <metainfo20@gmail.com>
2021-09-16 23:25:07 +0300

    remove sleep

--- test_items.py
+++ test_items.py
@@ -5,7 +5,6 @@ link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 def test_button_add_to_basket_is_exist(browser):
     browser.get(link)
-    time.sleep(30)
     assert browser.find_element_by_class_name(
         "btn-add-to-basket"
     ).is_displayed(), "Button add to basket is not exist"
