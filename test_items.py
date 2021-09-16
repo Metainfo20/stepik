47dec3f6de203efd970610ce20cc9537c5db3ff0
metainfo20 <metainfo20@gmail.com>
2021-09-16 23:05:12 +0300

    change url for test in test_items.py

--- test_items.py
+++ test_items.py
@@ -2,7 +2,7 @@ import time
 
 
 def test_button_add_to_basket_is_exist(browser):
-    browser.get("http://selenium1py.pythonanywhere.com/catalogue/hackers-painters_185/")
+    browser.get("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/")
     time.sleep(30)
     assert browser.find_element_by_class_name(
         "btn-add-to-basket"
